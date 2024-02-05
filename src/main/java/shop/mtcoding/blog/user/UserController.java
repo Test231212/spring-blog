f459c832ca2f1d2660f914cdeb231892f9b88710
test231212 <j910219@gmail.com>
2024-02-05 17:29:51 +0900

    회원 ì가입 ã완료

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,10 +1,24 @@
-package shop.mtcoding.blog.controller;
+package shop.mtcoding.blog.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor // final이 붙었을 때 생성자를 만들어줌
 @Controller
 public class UserController {
+
+    private final UserRepository userRepository;
+
+
+    @PostMapping("/join")
+    public String join(UserRequest.JoinDTO requestDTO) {
+        System.out.println(requestDTO);
+
+        userRepository.save(requestDTO);
+        return "redirect:/loginForm";
+    }
 
     @GetMapping("/joinForm")
     public String joinForm() {
