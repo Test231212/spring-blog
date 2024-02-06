3db2ba723e7de2ddeb7f89bfaef971f16300373a
test231212 <j910219@gmail.com>
2024-02-06 10:34:49 +0900

    로그아웃 구현완료

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -44,20 +44,25 @@ public class UserController {
         userRepository.save(requestDTO); // 모델에 위임하기
         return "redirect:/loginForm";
     }
+
     @GetMapping("/joinForm")
     public String joinForm() {
         return "user/joinForm";
     }
+
     @GetMapping("/loginForm")
     public String loginForm() {
         return "user/loginForm";
     }
+
     @GetMapping("/user/updateForm")
     public String updateForm() {
         return "user/updateForm";
     }
+
     @GetMapping("/logout")
     public String logout() {
+        session.invalidate();
         return "redirect:/";
     }
 }
